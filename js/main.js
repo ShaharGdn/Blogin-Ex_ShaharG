4cebd205734163601967b2d44ac93106e9e5b7f9
ShaharGdn <shahargadon@gmail.com>
2024-02-28 20:03:29 +0200

    Add modal for continue reading responsive design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+'use strict'
+
 function toggleMenu() {
     document.body.classList.toggle('menu-open');
+}
+
+function toggleModal() {
+    const elModal =  document.querySelector('.modal')
+    elModal.classList.toggle('hidden')
+    document.body.classList.toggle('modal-open')
 }
